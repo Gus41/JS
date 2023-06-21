102d8f1003a39d1d8e65b8c70b3b21def6bf8a3d
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-06-21 11:41:21 -0300

    .

--- 34/script.js
+++ 34/script.js
@@ -16,3 +16,13 @@ class Carro{
         this.cor = cor
     }
 }
+const caixa = document.getElementById("res")
+class Militar extends Carro{
+    constructor(nome,portas){
+        super(nome,portas)
+    }
+}
+const c2 = new Militar("Militar",6)
+const c1 = new Carro("normal",2)
+c2.setCor("Verde")
+caixa.innerHTML = c2.nome + " " + c2.portas + " " + c2.cor
