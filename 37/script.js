65f9e6b6387fe1fb4543665211b337487e2d6f34
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-06-27 11:28:44 -0300

    uptade

--- 37/script.js
+++ 37/script.js
@@ -1,2 +1,19 @@
 const teclasNum = [...document.querySelectorAll(".num")]
-const teclasOp = [...document.querySelectorAll(".op")]
+const teclasOp = [...document.querySelectorAll(".Top")]
+const res = document.getElementById("igual")
+const display = document.getElementById("saida")
+const OnOff = document.getElementById("OnOff")
+const clear = document.getElementById("clear")
+teclasNum.forEach((el)=>{
+    el.addEventListener("click",(evt)=>{
+        display.innerHTML+=evt.target.innerHTML
+    })
+})
+teclasOp.forEach((el)=>{
+    el.addEventListener("click",(evt)=>{
+        display.innerHTML+=evt.target.innerHTML
+    })
+})
+clear.addEventListener("click",()=>{
+    display.innerHTML = '0'
+})
