452ad6d2988f5716325a99f3cba0f05020d1f3cc
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-06-27 11:56:43 -0300

    calculadora finalizada

--- 37/script.js
+++ 37/script.js
@@ -4,16 +4,51 @@ const res = document.getElementById("igual")
 const display = document.getElementById("saida")
 const OnOff = document.getElementById("OnOff")
 const clear = document.getElementById("clear")
+
+let flag = false
+let virgula = false
+
 teclasNum.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
-        display.innerHTML+=evt.target.innerHTML
+        flag = false
+        if(evt.target.innerHTML == ","){
+            if(!virgula){
+                virgula = true
+                if(display.innerHTML == "0"){
+                    display.innerHTML = "0,"
+                }else{
+                    display.innerHTML += evt.target.innerHTML
+                }
+            }
+        }else{
+            virgula = false
+            if(display.innerHTML == "0"){
+                display.innerHTML = ""
+            }
+            display.innerHTML += evt.target.innerHTML
+        }
+
     })
 })
 teclasOp.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
-        display.innerHTML+=evt.target.innerHTML
+        if( !flag ){
+            flag = true
+            if( display.innerHTML == "0" ){
+                display.innerHTML= ""
+            }
+            display.innerHTML += evt.target.innerHTML
+        }
     })
 })
 clear.addEventListener("click",()=>{
+    flag = false
+    virgula = false
     display.innerHTML = '0'
+})
+res.addEventListener("click",()=>{
+    flag = false
+    virgula = false
+    const resultado = eval(display.innerHTML)
+    display.innerHTML = resultado;
 })
