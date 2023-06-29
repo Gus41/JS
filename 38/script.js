92616194e741b2953a3741966b9264c05f257ff5
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-06-29 12:41:52 -0300

    promise

--- 38/script.js
+++ 38/script.js
@@ -1,29 +1,38 @@
 const num = document.getElementById("num")
+const btn = document.getElementById("btn")
+num.innerHTML = "Esperando..."
+const promesa = ()=>{
+    let promise = new Promise((ok,rejected)=>{  //instanciando nova promessa
+        let resultado = false
+        let tempo = 2500
+        setTimeout( ()=>{
+            resultado = true
+            if(resultado){
+                ok("Deu tudo certo")
+            }else{
+                rejected("Deu tudo errado")
+            }
+        }, tempo )
+    
+    })
+    
+    promise.then((retorno)=>{
+        num.innerHTML = retorno
+        num.classList.remove("erro")
+        num.classList.add("ok")
+    })
+    promise.catch((retorno)=>{
+        num.innerHTML = retorno;
+        num.classList.add("erro")
+        num.classList.remove("ok")
+    })
+}
 
-let promise = new Promise((ok,rejected)=>{  //instanciando nova promessa
-    let resultado = false
-    let tempo = 2500
-    setTimeout( ()=>{
-        resultado = true
-        if(resultado){
-            ok("Deu tudo certo")
-        }else{
-            rejected("Deu tudo errado")
-        }
-    }, tempo )
-
+btn.addEventListener("click",()=>{
+    num.innerHTML = "Processando..."
+    promesa();
 })
 
-promise.then((retorno)=>{
-    num.innerHTML = retorno
-    num.classList.remove("erro")
-    num.classList.add("ok")
-})
-promise.catch((retorno)=>{
-    num.innerHTML = retorno;
-    num.classList.add("erro")
-    num.classList.remove("ok")
-})
 
-num.innerHTML = "Processando..."
+
 
