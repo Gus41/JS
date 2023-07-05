34393249be76c8954c73b9235394405996931f9d
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-07-05 16:03:51 -0300

    .

--- 44/script.js
+++ 44/script.js
@@ -4,3 +4,7 @@ const btn_remove = document.getElementById("btn_remove")
 const QntsObjsADD = document.getElementById("n")
 const btn_add = document.getElementById("btn_add")
 
+let largura_palco = palco.offsetWidth
+let altura_palco = palco.offsetHeight
+let elementos = []
+let num_elementos = 0
