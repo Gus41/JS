c53523e545a41bcff64754109d9337a8aed6fd86
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-07-05 15:59:32 -0300

    js

---
+++ 44/script.js
@@ -0,0 +1,6 @@
+const num_objs = document.getElementById("n_objs")
+const palco = document.getElementById("palco")
+const btn_remove = document.getElementById("btn_remove")
+const QntsObjsADD = document.getElementById("n")
+const btn_add = document.getElementById("btn_add")
+
