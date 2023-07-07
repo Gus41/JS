245465ac3e1c4578705c59e472c74164b89e57a5
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-07-07 14:47:39 -0300

    .

--- 47/script.js
+++ 47/script.js
@@ -1,13 +1,21 @@
 const alerta = document.getElementById("btn_alert")
 const conf = document.getElementById("btn_confirm")
 const promp = document.getElementById("btn_prompt")
-
+const conteudo = document.getElementById("main")
 alerta.addEventListener("click",()=>{
     alert("Janela de alerta")
 })
 conf.addEventListener("click",()=>{
-    confirm("Confirma aí po")
+    if (confirm("Gostaria de confirmar? ")){
+        alert("Confirmado")
+    }else{
+        alert("Nao confirmado")
+    }
 })
 promp.addEventListener("click",()=>{
-    prompt("Digita alguma coisa ai po")
+    const string = prompt("Digita alguma coisa ai po","Texto Padrão")
+    const div = document.createElement("div")
+    div.innerHTML = string
+    conteudo.appendChild(div)
+
 })
