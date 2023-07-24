8dd060029bdccdc28d2b963d8c6526c53eba7695
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-07-24 20:40:41 -0300

    Update script.js

--- 53/script.js
+++ 53/script.js
@@ -3,3 +3,10 @@ const pessoas = [
     {id:2,nome:"Maria"},
     {id:1,nome:"Helena"},
 ]
+const novasPessoas = new Map()
+
+for( pes of pessoas ){
+    const {id} = pes
+    novasPessoas.set(id,{...pes})
+}
+console.log(novasPessoas)
