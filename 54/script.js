12bee76aeff56b39e56901a7f721366f98de4292
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-07-25 19:10:45 -0300

    Update script.js

--- 54/script.js
+++ 54/script.js
@@ -16,6 +16,40 @@ class Pessoa{
     }
 }
 const p1 = new Pessoa("Otavio","Alberto")
-console.log(p1)
-p1.beber()
-p1.falar()
+
+//---------------------------
+const _cvelocidade = Symbol("velocidade") //gera um id aleatorio
+console.log(_cvelocidade)
+class Carro{
+    constructor(nome){
+        this.nome = nome
+        this[_cvelocidade] = 0
+    }
+    set velocidade(n){
+        if( typeof(n) !== "number" ){
+            console.log("Valor invalido")
+            return
+        }
+        if( n > 100 || n < 0 ){
+            console.log("Valor invalido")
+            return
+        }
+        this[_cvelocidade] = n
+    }
+    acelerar(){
+        if( this[_cvelocidade] < 100 ){
+            this[_cvelocidade]++
+        }
+    }
+    freiar(){
+        if( this[_cvelocidade] > 0 ){
+            this[_cvelocidade]--
+        }
+    }
+    get velocidade(){
+        return this[_cvelocidade]
+    }
+}
+const fusca = new Carro("Fusca")
+fusca.velocidade = 200
+console.log(fusca)
