0cd6d0c0b18a02261134bc6c5753d5b2098f98d6
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-07-28 17:40:34 -0300

    Update script.js

--- 61/script.js
+++ 61/script.js
@@ -1,4 +1,4 @@
-function rand(min,max){
+function rand(min=0,max=3){
     min *= 1000
     max *= 1000
 
@@ -16,4 +16,35 @@ function espera(msg,tempo){
         },tempo)
     })
 }
-espera("")
+//espera("Fase1",rand(0,3)).then(dado=>{
+//    console.log(dado)
+//    return espera("Fase2",rand()).then(dado=>{
+//        console.log(dado)
+//        return espera("Fase3",rand()).then(dado=>{
+///            console.log(dado)
+//            return dado
+//        }).then(e=>{
+//            console.log("Terminado na fase: " + e)
+//       })
+//    })
+//}).catch(err=>{
+//    console.log(err)
+//})
+//--------------------------------------------------------------
+async function execute(){
+    try{
+        const fase1 = await espera("Fase1-2",rand())
+        console.log(fase1)
+        const fase2 = await espera(5,rand())
+        console.log(fase2)
+        const fase3 = await espera("Fase3-2",rand())
+        console.log(fase3)
+        console.log("Finalizado na " + fase3)
+
+    }catch(err){
+        console.log(err)
+        
+    }
+    
+}
+execute(0)
