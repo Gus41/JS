e72308d1a98d467492becced4b450f51ef0e3dcb
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-07-28 17:20:55 -0300

    Update script.js

--- 60/script.js
+++ 60/script.js
@@ -7,18 +7,35 @@ function rand(min,max){
 
 function epseraAi(msg,tempo){
     return new Promise( (resolve,reject)=>{
-        if( typeof(msg) !== "string" ){
-            reject(false)
-        }
 
         setTimeout(()=>{
+            if( typeof(msg) !== "string" ){
+            reject("Erro de tipagem")
+            return
+        }
             resolve(msg.toUpperCase() + " Passou na promise")
         },tempo)
     } )
 }
-const promises = ["PrimeiroValor",epseraAi("PrimeiraPromise",3000),epseraAi("Segunda Promise",5000),epseraAi("UltimoValor",1000)]
-Promise.all(promises).then(dados => {
-    console.log(dados)
+//const promises = [epseraAi("PrimeiraPromise",3000),epseraAi("Segunda Promise",5000),epseraAi(1,20000),epseraAi("UltimoValor",1000)]
+//Promise.race(promises).then(dados => {
+//    console.log(dados)
+//}).catch(err=>{
+//    console.log(err)
+//    //tratar erro
+//})
+
+function baixaPagina(){
+    let emCache = false
+
+    if(emCache){
+        return Promise.resolve("Pagina em cache")
+    }else{
+        return epseraAi("Pagina baixada",rand(1,9))
+    }
+}
+baixaPagina().then((msg)=>{
+    console.log(msg)
 }).catch(err=>{
-    console.log("Erro")
+    console.log(err)
 })
