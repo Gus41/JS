f255e8536d059da70efba3b2e03ca4228cd87288
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-07-29 14:23:09 -0300

    .

--- 62/script.js
+++ 62/script.js
@@ -1,26 +1,47 @@
 const request = obj =>{
-    const xhr = new XMLHttpRequest()  //xml http request
-    xhr.open(obj.method,obj.url,true)
-    xhr.send() // post
 
-
-    xhr.addEventListener("load",()=>{ // será executado quando tiver recebido a resposta do servidor
-        // entre 200 e 300 === aceitar
-        if( xhr.status >= 200 && xhr.status <= 300 ){
-            //requesição de sucesso
-            obj.succes(xhr.responseText)
-        }else{
-            // requesição falhou
-            obj.error(xhr.statusText)
-        }
+    return new Promise((resolve,reject)=>{
+        const xhr = new XMLHttpRequest()  //xml http request
+        xhr.open(obj.method,obj.url,true)
+        xhr.send() // post
+    
+    
+        xhr.addEventListener("load",()=>{ // será executado quando tiver recebido a resposta do servidor
+            // entre 200 e 300 === aceitar
+            if( xhr.status >= 200 && xhr.status <= 300 ){
+                //requesição de sucesso
+                resolve(xhr.responseText)
+            }else{
+                // requesição falhou
+                reject(xhr.statusText)
+            }
+        })
     })
+}
+function loadResult(response){
+    const saida = document.getElementById("res")
+    saida.innerHTML = response
+}
+async function carregdaPagina(elemento){
+    const href = elemento.getAttribute("href")
+    const objConfig = {
+        method:"GET",
+        url: href
+    };
+    try{
+        const response = await request(objConfig)    
+        loadResult(response)
+    }catch(err){
+        alert(err)
+    }
 }
 document.addEventListener("click",(e)=>{
     const el = e.target
     const tag = el.tagName.toLowerCase()
     if( tag === "a" ){
+        e.preventDefault()
         // é o link
-        
+        carregdaPagina(el)
 
     }
 })
