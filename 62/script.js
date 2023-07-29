66cbcedc6f07f87b17c630c0b661265fdd9d07ae
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-07-29 13:55:08 -0300

    .

--- 62/script.js
+++ 62/script.js
@@ -4,7 +4,23 @@ const request = obj =>{
     xhr.send() // post
 
 
-    xhr.addEventListener("load",()=>{
-        
+    xhr.addEventListener("load",()=>{ // será executado quando tiver recebido a resposta do servidor
+        // entre 200 e 300 === aceitar
+        if( xhr.status >= 200 && xhr.status <= 300 ){
+            //requesição de sucesso
+            obj.succes(xhr.responseText)
+        }else{
+            // requesição falhou
+            obj.error(xhr.statusText)
+        }
     })
 }
+document.addEventListener("click",(e)=>{
+    const el = e.target
+    const tag = el.tagName.toLowerCase()
+    if( tag === "a" ){
+        // é o link
+        
+
+    }
+})
