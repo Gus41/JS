0d455cb74a093e91873ca955973115832ac50cd3
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-07-30 15:23:58 -0300

    .

--- 64/script.js
+++ 64/script.js
@@ -1,4 +1,9 @@
-const nome = "Teste"
-const obj = {nome}
-const novoObj = {...obj}
-console.log(novoObj)
+class Pessoa{
+    constructor(nome,sobrenome){
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+}
+const pessoa1 = new Pessoa("Alberto","Andre")
+
+//npm run babel
