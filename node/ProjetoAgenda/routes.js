0eaa24b932794685620355e896903b5e7d79cb0b
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-08-25 18:25:16 -0300

    .

---
+++ node/ProjetoAgenda/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController.js');
+
+// Rotas da home
+route.get('/', homeController.paginaInicial);
+
+// Rotas de login
+route.get("/login/index",loginController.index)
+
+
+module.exports = route;
